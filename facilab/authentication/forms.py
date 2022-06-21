55fcd6e7c46b4d72c0aa974727b2e90ee0e9a434
Elodie Riou <elodie.riou@hotmail.com>
2022-06-21 16:38:41 +0200

    add page login, list request, detail request

--- facilab/authentication/forms.py
+++ facilab/authentication/forms.py
@@ -7,6 +7,11 @@ class LoginForm(forms.Form):
     email = forms.EmailField(label='Votre identifiant (adresse mail)')
     password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Votre mot de passe')
 
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs.update({'class': 'input-line full-width'})
+        self.fields['password'].widget.attrs.update({'class': 'input-line full-width'})
+
 
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
